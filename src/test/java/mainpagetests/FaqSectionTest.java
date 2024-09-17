aadeb587baefb9b6b4a5cf39ee1bba6c83b4262d
Pavel Shpartko <workmail.sp.one@gmail.com>
2024-09-17 18:55:51 +0300

    and another improvements

--- src/test/java/mainpagetests/FaqSectionTest.java
+++ src/test/java/mainpagetests/FaqSectionTest.java
@@ -3,6 +3,8 @@ package mainpagetests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,20 +12,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pageobject.MainPage;
 
-import java.util.HashMap;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class FaqSectionTest {
 
     private ChromeOptions options;
     private WebDriver driver;
     private MainPage mainPage;
     private WebDriverWait w8;
-    List<WebElement> questionElements;
-    List<WebElement> answerElements;
-    HashMap<String, String> answerQuestionMap;
+    private final String questionText;
+    private final String expectedAnswerText;
+
+    public FaqSectionTest(String questionText, String expectedAnswerText) {
+        this.questionText = questionText;
+        this.expectedAnswerText = expectedAnswerText;
+    }
 
     @Before
     public void presetting() {
@@ -31,184 +35,35 @@ public class FaqSectionTest {
         driver = new ChromeDriver(options);
         driver.get(MainPage.MAIN_PAGE_URL);
         mainPage = new MainPage(driver);
-        w8 = new WebDriverWait(driver, 3);
-        questionElements = mainPage.getFaqQuestionsList();
-        answerElements = mainPage.getAnswersList();
-        answerQuestionMap = mainPage.getAnswerQuestionMap();
+        w8 = new WebDriverWait(driver, 5);
     }
-    // ВАРИАНТ 1 -------------------------------------------------------------------------------------------------------
-/*    @Test
-    public void answersShouldOpenAfterClickAtQuestion() {
-        for (int i = 0; i < questionElements.size(); i++) {
-            WebElement question = questionElements.get(i);
-            WebElement answer = answerElements.get(i);
-            mainPage.scrollToElement(question);
-            question.click();
-            try {
-                w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-            } catch (TimeoutException e) {
-                System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-                continue;
-            }
-            System.out.printf("[%d]Вопрос: %s\n[%d]Ответ: %s\n", i, question.getText(), i, answer.getText());
-            assertEquals(
-                    String.format("Ответ не соотвествует вопросу[%d]", i),
-                    answerQuestionMap.getOrDefault(question.getText(), ""),
-                    answer.getText()
-            );
-            System.out.println("-------------------------------------------------------------------------------------");
-        }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
     }
-*/
-    // ВАРИАНТ 2 -------------------------------------------------------------------------------------------------------
+
     @Test
     public void firstAnswerShouldCorrespondFirstQuestion() {
-        WebElement question = questionElements.get(0);
-        WebElement answer = answerElements.get(0);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
+        WebElement questionElement = mainPage.getQuestionElementWithText(questionText);
+        mainPage.scrollToElement(questionElement);
+        questionElement.click();
+        w8.until(ExpectedConditions.attributeToBe(questionElement, "aria-expanded", "true"));
+        WebElement answerElement = mainPage.getExpandedAnswer();
+        w8.until(ExpectedConditions.visibilityOf(answerElement));
         assertEquals(
                 "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void secondAnswerShouldCorrespondSecondQuestion() {
-        WebElement question = questionElements.get(1);
-        WebElement answer = answerElements.get(1);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-    @Test
-    public void thirdAnswerShouldCorrespondThirdQuestion() {
-        WebElement question = questionElements.get(2);
-        WebElement answer = answerElements.get(2);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void fourthAnswerShouldCorrespondFourthQuestion() {
-        WebElement question = questionElements.get(3);
-        WebElement answer = answerElements.get(3);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void fifthAnswerShouldCorrespondFifthQuestion() {
-        WebElement question = questionElements.get(4);
-        WebElement answer = answerElements.get(4);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void sixthAnswerShouldCorrespondSixthQuestion() {
-        WebElement question = questionElements.get(5);
-        WebElement answer = answerElements.get(5);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void seventhAnswerShouldCorrespondSeventhQuestion() {
-        WebElement question = questionElements.get(6);
-        WebElement answer = answerElements.get(6);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
-        );
-    }
-
-    @Test
-    public void eightAnswerShouldCorrespondEightQuestion() {
-        WebElement question = questionElements.get(7);
-        WebElement answer = answerElements.get(7);
-        mainPage.scrollToElement(question);
-        question.click();
-        try {
-            w8.until(ExpectedConditions.attributeToBe(question, "aria-expanded", "true"));
-        } catch (TimeoutException e) {
-            System.out.printf("Ответ на вопрос \"%s\" не раскрылся.\n", question.getText());
-        }
-        System.out.printf("Вопрос: %s\nОтвет: %s\n",question.getText(), answer.getText());
-        assertEquals(
-                "Ответ не соотвествует вопросу",
-                answerQuestionMap.getOrDefault(question.getText(), ""),
-                answer.getText()
+                expectedAnswerText,
+                answerElement.getText()
         );
     }
 
--- src/test/java/pageobject/MainPage.java
+++ src/test/java/pageobject/MainPage.java
@@ -4,20 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 public class MainPage implements Scrollable {
 
     private final WebDriver driver;
-    public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
-    private static final By FAQ_QUESTIONS_LIST = By.xpath(".//div[@aria-expanded]");
-    private static final By FAQ_ANSWERS_LIST = By.xpath(".//div[@class='accordion__panel']");
+    public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru";
+    private static final By FAQ_EXPANDED_ANSWER = By.xpath(".//div[@class='accordion__panel' and not(@hidden)]");
     private static final By TOP_ORDER_BUTTON = By.xpath(".//button[@class='Button_Button__ra12g']");
     private static final By BOT_ORDER_BUTTON = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button");
 
@@ -25,12 +17,13 @@ public class MainPage implements Scrollable {
         this.driver = driver;
     }
 
-    public List<WebElement> getFaqQuestionsList() {
-        return driver.findElements(FAQ_QUESTIONS_LIST);
+    public WebElement getQuestionElementWithText(String questionText) {
+        By currentQuestion = By.xpath(String.format(".//div[@aria-expanded and text()='%s']", questionText));
+        return driver.findElement(currentQuestion);
     }
 
-    public List<WebElement> getAnswersList() {
-        return driver.findElements(FAQ_ANSWERS_LIST);
+    public WebElement getExpandedAnswer() {
+        return driver.findElement(FAQ_EXPANDED_ANSWER);
     }
 
     public WebElement getTopOrderButton() {
@@ -47,29 +40,6 @@ public class MainPage implements Scrollable {
 
     public void clickBotOrderButton() {
         getBotOrderButton().click();
-    }
-
-    public HashMap<String, String> getAnswerQuestionMap() { // Дальше бога нет
-        WebDriverWait w8 = new WebDriverWait(driver, 3);
-        List<String> questionsList = driver.findElements(FAQ_QUESTIONS_LIST)
-                .stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-        List<String> answersList = driver.findElements(FAQ_ANSWERS_LIST)
-                .stream()
-                .map(element -> {
-                    ((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute(arguments[1]);", element, "hidden");
-                    w8.until(ExpectedConditions.visibilityOf(element));
-                    String text = element.getText();
-                    ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);", element, "hidden", "");
-                    return text;
-                })
-                .collect(Collectors.toList());
-        HashMap<String, String> answerQuestionMap = new HashMap<>();
-        for (int i = 0; i < questionsList.size(); i++) {
-            answerQuestionMap.put(questionsList.get(i), answersList.get(i));
-        }
-        return answerQuestionMap;
     }
 
     @Override
